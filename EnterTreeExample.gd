2ff9c78ffc26a9a3dc2f8738311f8565615a2e5d
Godot Tutorials <56376603+Godot-Tutorials@users.noreply.github.com>
2020-06-15 20:31:34 -1000

    update comments

--- EnterTreeExample.gd
+++ EnterTreeExample.gd
@@ -3,4 +3,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _enter_tree():
+	#prints to console the name of the Node
 	print(self.get_name(), " has entered the SceneTree")
